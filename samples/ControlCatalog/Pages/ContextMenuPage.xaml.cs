2a20557c6c8c6d427ba756d70df061e4cfa45f60
Dariusz Komosinski <darek.komosinski@gmail.com>
2019-02-22 14:17:21 +0100

    Try generating context menu items dynamically.

--- samples/ControlCatalog/Pages/ContextMenuPage.xaml.cs
+++ samples/ControlCatalog/Pages/ContextMenuPage.xaml.cs
@@ -8,6 +8,52 @@ namespace ControlCatalog.Pages
         public ContextMenuPage()
         {
             this.InitializeComponent();
+
+            var vm = new MenuPageViewModel();
+
+            vm.MenuItems = new[]
+            {
+                new MenuItemViewModel
+                {
+                    Header = "_File",
+                    Items = new[]
+                    {
+                        new MenuItemViewModel { Header = "_Open...", Command = vm.OpenCommand },
+                        new MenuItemViewModel { Header = "Save", Command = vm.SaveCommand },
+                        new MenuItemViewModel { Header = "-" },
+                        new MenuItemViewModel
+                        {
+                            Header = "Recent",
+                            Items = new[]
+                            {
+                                new MenuItemViewModel
+                                {
+                                    Header = "File1.txt",
+                                    Command = vm.OpenRecentCommand,
+                                    CommandParameter = @"c:\foo\File1.txt"
+                                },
+                                new MenuItemViewModel
+                                {
+                                    Header = "File2.txt",
+                                    Command = vm.OpenRecentCommand,
+                                    CommandParameter = @"c:\foo\File2.txt"
+                                },
+                            }
+                        },
+                    }
+                },
+                new MenuItemViewModel
+                {
+                    Header = "_Edit",
+                    Items = new[]
+                    {
+                        new MenuItemViewModel { Header = "_Copy" },
+                        new MenuItemViewModel { Header = "_Paste" },
+                    }
+                }
+            };
+
+            DataContext = vm;
         }
 
         private void InitializeComponent()
